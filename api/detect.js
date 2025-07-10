ef3ed1b0c27cc43b1c8cd98c014fd510cac6ed98
sowbhakyaakaalo <sowbhakyaa@kaalo.com>
2025-07-10 12:24:42 +0530

    detect.js

--- api/detect.js
+++ api/detect.js
@@ -1,1 +1,34 @@
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
+  const { readable, writable } = new TransformStream();
+  const formData = await req.formData();
+  const file = formData.get('file');
+
+  const body = new FormData();
+  body.set('model', process.env.ULTRA_MODEL_URL);
+  body.set('imgsz', 640);
+  body.set('conf', 0.25);
+  body.set('iou', 0.45);
+  body.set('file', file);
+
+  const response = await fetch('https://predict.ultralytics.com', {
+    method: 'POST',
+    headers: {
+      'x-api-key': process.env.ULTRA_API_KEY
+    },
+    body: body
+  });
+
+  const result = await response.text();
+  const writer = writable.getWriter();
+  writer.write(new TextEncoder().encode(result));
+  writer.close();
+
+  return new Response(readable, {
+    headers: { "Content-Type": "application/json" }
+  });
+}
 
