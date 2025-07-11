4779acc76581c2bddedcd182813d8341fa7af1a2
sowbhakyaakaalo <sowbhakyaa@kaalo.com>
2025-07-11 13:02:03 +0530

    detect.js

--- api/detect.js
+++ api/detect.js
@@ -1,20 +1,40 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+import formidable from 'formidable';
+import fs from 'fs';
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const formData = new FormData();
-    formData.append('file', req.body);
+    const form = new formidable.IncomingForm();
 
-    const response = await fetch("https://predict.ultralytics.com", {
-      method: 'POST',
-      headers: {
-        'x-api-key': process.env.ULTRA_API_KEY
-      },
-      body: req
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error parsing the file.' });
+      }
+
+      const file = fs.createReadStream(files.file[0].filepath);
+
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch("https://predict.ultralytics.com", {
+        method: 'POST',
+        headers: {
+          'x-api-key': process.env.ULTRA_API_KEY
+        },
+        body: formData
+      });
+
+      const data = await response.json();
+      res.status(200).json(data);
     });
 
-    const data = await response.json();
-    res.status(200).json(data);
   } else {
     res.status(405).send('Method Not Allowed');
   }
 }
-
